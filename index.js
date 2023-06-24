cbe97665bde8fd1816fe2671c67b1b399001cbcb
brewek <github@the226.pl>
2023-06-24 20:02:45 +0200

    various fixes

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ get_user().catch(err => {
 });
 
 
+var last_known_game
 get_last_game().then(last_game => {
     update_page(last_game)
+    last_known_game = last_game
 }).catch(err => { console.error(err) });
 
 function prettyDate(date) {
@@ -18,33 +20,33 @@ function prettyDate(date) {
     return date.toDateString();
 }
 
-function update_page(last_game) {
-    if (last_game.game.url.indexOf("lichess.org") > -1) {
+function update_page(game) {
+    if (game.game.url.indexOf("chess.com") > -1) {
         lichess_import_button = document.getElementById("lichess_import");
-        lichess_import_button.disabled = true
+        lichess_import_button.disabled = false
     }
 
     title = document.getElementById("title");
-    title.href = last_game.game.url
-    white = last_game.game.players.white
-    black = last_game.game.players.black
-    game_ended = new Date(last_game.game.game_ended)
+    title.href = game.game.url
+    white = game.game.players.white
+    black = game.game.players.black
+    game_ended = new Date(game.game.game_ended)
     title.innerHTML = `${prettyDate(game_ended)}: ${white.username} (${white.rating}) vs ${black.username} (${black.rating})`
-    recreate_board(last_game);
+    recreate_board(game);
 }
 
-function recreate_board(last_game) {
-    var board = Chessboard2("chessboard", last_game.FEN);
-    if (!last_game.game.viewer) {
+function recreate_board(game) {
+    var board = Chessboard2("chessboard", game.FEN);
+    if (!game.game.viewer) {
         board.flip()
     }
     board.addArrow({
         color: 'red',
-        start: last_game.MovePlayed.substring(0, 2),
-        end: last_game.MovePlayed.substring(2),
+        start: game.MovePlayed.substring(0, 2),
+        end: game.MovePlayed.substring(2),
         opacity: 0.35
     })
-    for (move of last_game.ExpectedMoves) {
+    for (move of game.ExpectedMoves) {
         board.addArrow({
             color: 'green',
             start: move.substring(0, 2),
@@ -65,17 +67,29 @@ async function get_last_game() {
     return await response.json()
 }
 
-function import_last_game() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            window.open(xhr.responseText);
-        }
-        else {
-            console.log(xhr.responseText)
-        }
+$('#lichess_import').value = "asd"
+$('#lichess_import').on('click', function () {
+    import_game(last_known_game).then(url => {
+        window.open(url);
+    }).catch(err => {
+        log.error(err);
+    })
+    return false;
+});
+
+async function import_game(game) {
+
+    let response = await fetch(`${lambda_url}/import`, {
+        method: "POST",
+        headers: {
+            Authorization: `${token.token_type} ${token.access_token}`,
+            "Content-Type": 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        body: game.game.pgn
+    })
+
+    if (!response.ok) {
+        throw new Error(await response.text());
     }
-    xhr.open('post', `${lambda_url}/import`, false);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    xhr.send(last_game.game.pgn);
+    return response.text()
 }
