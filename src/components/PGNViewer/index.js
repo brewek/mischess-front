3bf0b5663cb2a4fc2a21b713acb75c8b3f6a3df2
Kaan Kara <kaan_k193@hotmail.com>
2023-06-29 22:55:27 +0000

    add pgn

---
+++ src/components/PGNViewer/index.js
@@ -0,0 +1,115 @@
+import {
+  Grid, 
+  List, 
+  ListItem, 
+  ListItemButton, 
+  ListItemText, 
+  Typography
+} from '@mui/material';
+import { Chess } from 'chess.js';
+import { useEffect, useState } from 'react';
+
+export default function PGNViewer(props) {
+  const [history, setHistory] = useState([]);
+
+  const changeFen = (idx) => {
+    props.setGameConfig({
+      ...props.gameConfig,
+      position: history[idx].fen
+    });
+    props.clearArrows();
+  }
+
+  useEffect(() => {
+    console.log(props.pgn);
+    
+    if (!props.pgn)
+      return () => {};
+
+    let chess = new Chess()
+    chess.loadPgn(props.pgn);
+    console.log(chess.history());
+
+    let tempChess = new Chess()
+    let moves = chess.history().map((move, idx) => {
+      tempChess.move(move);
+      let fen = tempChess.fen()
+      return {
+        move,
+        fen
+      }
+    });
+    tempChess = null // let instance to be collected by garbage collector
+    setHistory(moves);
+
+    return () => {
+      chess = null;
+    };
+  }, [props.pgn, props.height])
+  
+  return (
+    <Grid container spacing={2} style={{
+      height: props.height,
+      overflowX: 'hidden',
+      overflowY: 'scroll',
+      marginTop: '1px'
+    }}>
+      <Grid item xs={6}>
+        <div style={{
+          textAlign: 'center',
+          overflow: 'hidden'
+        }}>
+          <Typography
+            align='center' 
+            variant='caption' 
+          >
+        
+            {props.players ? props.players.white.username : null}
+          </Typography>
+        </div>
+        <List dense>
+          {history ? history.filter((item, idx) => {
+            return idx % 2 === 0;
+          }).map((item, idx) => {
+            return (
+              <ListItem key={`odd-${idx}`}>
+                <ListItemButton onClick={() => changeFen(idx)} key={idx} >
+                  <ListItemText>
+                    {item.move}    
+                  </ListItemText>
+                </ListItemButton>          
+              </ListItem >
+          )}) : null}
+        </List>
+      </Grid>
+      <Grid item xs={6}>
+      <div style={{
+        textAlign: 'center',
+        overflow: 'hidden'
+      }}>
+          <Typography
+            align='center' 
+            variant='caption' 
+          >
+        
+            {props.players ? props.players.black.username : null}
+          </Typography>
+        </div>
+        <List dense>
+          {history ? history.filter((item, idx) => {
+            return idx % 2 !== 0;
+          }).map((item, idx) => {
+            return (
+              <ListItem key={`even-${idx}`}>
+                <ListItemButton onClick={() => changeFen(idx)} key={idx} >
+                  <ListItemText>
+                    {item.move}    
+                  </ListItemText>
+                </ListItemButton>          
+              </ListItem >
+            )}) : null}
+        </List>
+      </Grid>
+    </Grid>
+  )
+}
